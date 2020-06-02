e67c5a79225a8b2253c825e6f3669b20f3187219
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-02 14:39:28 -0400

    variables

---
+++ liri.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+console.log(
+  " \n Hi, I'm Liri! What do you want to do today? \n Choose from the following: 'my-tweets', 'movie-this', 'spotify-this-song' or 'do-what-it-says'"
+);
+
+var keys = require("./keys.js");
+
+// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
+
+//Arguments provided
+var action = process.argv[2];
+var input = process.argv[3];
+var nodeArgs = process.argv;
+
+console.log("Command chosen: " + action);
+
+//Switch-case statement for different commands
+switch (action) {
+
+    case "concert-this":
+        concert();
+        break;
+
+	case "spotify-this-song":
+		song();
+		break;
+
+	case "movie-this":
+		movie();
+		break;
+
+	case "do-what-it-says":
+		random();
+		break;
+};
