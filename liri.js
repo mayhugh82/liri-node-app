ac1498478279e069edac8502f35c458a2bf00a31
Diana Mayhugh <dianamayhugh@gmail.com>
2020-06-04 19:06:26 -0400

    fix functions

--- liri.js
+++ liri.js
@@ -1,16 +1,18 @@
 //requires and linked files
 require("dotenv").config();
+
+var keys = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
+
 var fs = require("fs");
 var moment = require("moment");
 var axios = require("axios");
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
 
 //Arguments provided
 var userInput = process.argv;
-var inputTopic = process.argv[2];
+var action = process.argv[2];
 
 console.log("Command chosen: " + action);
 
@@ -36,14 +38,46 @@ switch (action) {
 //functions
 
 function bandInfo() {
-  var bandName = "";
+
+	var artist = "";
   for (var i = 3; i < userInput.length; i++) {
     if (i > 3 && i < userInput.length) {
-      bandName = bandName + "+" + userInput[i];
+      artist = artist + "+" + userInput[i];
     } else {
-      bandName += userInput[i];
+      artist += userInput[i];
     }
   }
+
+	var queryURL =
+    "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  console.log(queryURL);
+
+  axios.get(queryURL).then(function (bandResponse) {
+    console.log("Venue: " + bandResponse.data[0].venue.name);
+    console.log("City: " + bandResponse.data[0].venue.city);
+    console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+  })
+  .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
 
 function songInfo() {
@@ -55,6 +89,25 @@ function songInfo() {
       songName += userInput[i];
     }
   }
+  var spotify = new Spotify({
+    id: spotifyKeyInfo["spotify"].id,
+    secret: spotifyKeyInfo["spotify"].secret,
+  });
+
+  spotify.request(
+    "https://api.spotify.com/v1/search?q=track:" +
+      songName +
+      "&type=track&limit=10",
+    function (error, songResponse) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
+      console.log("Song: " + songResponse.tracks.items[0].name);
+      console.log("URL: " + songResponse.tracks.items[0].preview_url);
+      console.log("Album: " + songResponse.tracks.items[0].album.name);
+    }
+  );
 }
 
 function movieInfo() {
@@ -66,6 +119,19 @@ function movieInfo() {
       movieName += userInput[i];
     }
   }
+  var queryURL =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=[key]";
+
+  axios.get(queryURL).then(function (movieResponse) {
+    console.log("Title: " + movieResponse.data.Title);
+    console.log("Year: " + movieResponse.data.Year);
+    console.log("Rated: " + movieResponse.data.imdbRating);
+    console.log("Country: " + movieResponse.data.Country);
+    console.log("Language: " + movieResponse.data.Language);
+    console.log("Plot: " + movieResponse.data.Plot);
+    console.log("Actors: " + movieResponse.data.Actors);
+    console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+  });
 }
 
 function doWhatInfo() {
@@ -80,49 +146,8 @@ function doWhatInfo() {
   });
 }
 
-var queryURL =
-  "https://rest.bandsintown.com/artists/" +
-  bandName +
-  "/events?app_id=codingbootcamp";
 
-console.log(queryURL);
 
-axios.get(queryURL).then(function (bandResponse) {
-  console.log("Venue: " + bandResponse.data[0].venue.name);
-  console.log("City: " + bandResponse.data[0].venue.city);
-  console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
-});
 
-var spotify = new Spotify({
-  id: spotifyKeyInfo["spotify"].id,
-  secret: spotifyKeyInfo["spotify"].secret,
-});
 
-spotify.request(
-  "https://api.spotify.com/v1/search?q=track:" +
-    songName +
-    "&type=track&limit=10",
-  function (error, songResponse) {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
-    console.log("Song: " + songResponse.tracks.items[0].name);
-    console.log("URL: " + songResponse.tracks.items[0].preview_url);
-    console.log("Album: " + songResponse.tracks.items[0].album.name);
-  }
-);
 
-var queryURL =
-  "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=[key]";
-
-axios.get(queryURL).then(function (movieResponse) {
-  console.log("Title: " + movieResponse.data.Title);
-  console.log("Year: " + movieResponse.data.Year);
-  console.log("Rated: " + movieResponse.data.imdbRating);
-  console.log("Country: " + movieResponse.data.Country);
-  console.log("Language: " + movieResponse.data.Language);
-  console.log("Plot: " + movieResponse.data.Plot);
-  console.log("Actors: " + movieResponse.data.Actors);
-  console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
-});
